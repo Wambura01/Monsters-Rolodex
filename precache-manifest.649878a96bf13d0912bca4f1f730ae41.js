223dc097f21c0a4b55bdb88554a54f30697af0e4
Wambura <brianwambura96@gmail.com>
2020-03-06 19:26:50 +0300

    Updates

---
+++ precache-manifest.649878a96bf13d0912bca4f1f730ae41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370789a9726b0d3da1de34378ef87130",
+    "url": "/github.io/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "9159b8ec8161820b481c",
+    "url": "/github.io/Monsters-Rolodex/static/css/main.2a4dcaf6.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/github.io/Monsters-Rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github.io/Monsters-Rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9159b8ec8161820b481c",
+    "url": "/github.io/Monsters-Rolodex/static/js/main.780be065.chunk.js"
+  },
+  {
+    "revision": "37ebfe808e6c8792a6e6",
+    "url": "/github.io/Monsters-Rolodex/static/js/runtime-main.e42cc7f6.js"
+  }
+]);
